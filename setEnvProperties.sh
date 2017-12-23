7d2da168b30b4c511ae6cae5c74c4dea0aa260a2
Michal Sierzputowski <m.sierzputowski@gmail.com>
2017-12-23 16:25:56 +0000

    changed to local assets

--- setEnvProperties.sh
+++ setEnvProperties.sh
@@ -1,7 +1,7 @@
 #Properties for postgresqls
 export DB_DRIVER=org.postgresql.Driver
-export DB_USERNAME=michals
+export DB_USERNAME=budgetuser
 export DB_PASSWORD=""
-export DB_URL=jdbc:postgresql://localhost:5432/budgetapp
+export DB_URL=jdbc:postgresql://localhost:5432/budget4
 export DB_DIALECT=io.budgetapp.hibernate.dialect.CustomPostgreSQLDialect
 export DB_VALIDATE_QUERY="SELECT 1"
